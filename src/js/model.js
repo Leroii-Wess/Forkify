2127e5587c71ee83437c38c73419153ac494e8f0
Leroii-Wess <roywesley33@gmail.com>
2023-08-24 19:38:34 +0100

    Loading and Rendering Recipe

---
+++ src/js/model.js
@@ -0,0 +1,31 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const { recipe } = data.data;
+
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      sourceUrl: recipe.source_url,
+      publisher: recipe.publisher,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
