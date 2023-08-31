b06261e6d787f11a7d2dec779c045e6ea4e2233f
Leroii-Wess <roywesley33@gmail.com>
2023-08-31 17:11:39 +0100

    Complete Pagination Implementation

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,6 +5,7 @@ import * as model from './model';
 import recipeView from './views/recipeView';
 import searchView from './views/searchView';
 import resultsView from './views/resultsView';
+import paginationView from './views/paginationView';
 
 // if (module.hot) {
 //   module.hot.accept();
@@ -45,17 +46,26 @@ const controlSearchResult = async function () {
     await model.loadSearchResult(query);
 
     // 3. Render search
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
 
     // Render initial pagination
-    resultsView.render(model.getSearchResultsPage(3));
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // 1. Render New Search results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // Render NEW pagination
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipe);
   searchView.addHandlerSearch(controlSearchResult);
+  paginationView.addHandlerPagination(controlPagination);
 };
 init();
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,8 +1,78 @@
 import View from './View';
 import icons from 'url:../../img/icons.svg';
 
-class ResultsView extends View {
+class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
-  _generateMarkup() {}
+  addHandlerPagination(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    const curPage = this._data.page;
+
+    // page 1 and others
+    if (curPage === 1 && numPages > 1) {
+      return `
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+    }
+
+    // other page
+    if (curPage < numPages) {
+      return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+    }
+
+    // last page
+    if (curPage === numPages && numPages > 1) {
+      return `
+      <button data-goto="${
+        curPage - 1
+      }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+      `;
+    }
+
+    // only 1 page
+    return '';
+  }
 }
+
+export default new PaginationView();
